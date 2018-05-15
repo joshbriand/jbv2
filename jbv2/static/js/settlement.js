d41318dfa224f80a6db46c62672f885bb2a12639
Josh Briand <joshbriand@gmail.com>
2018-05-15 14:27:49 -0700

    cc fee take 1

--- jbv2/static/js/settlement.js
+++ jbv2/static/js/settlement.js
@@ -7,12 +7,17 @@ $(document).ready(function(){
 	function calculate() {
 		console.log('calculate');
 		gross = $('#gross').val();
-		temp_adjusted = ((gross / 1.12));
-		console.log(temp_adjusted);
-		adjusted = parseFloat(temp_adjusted);
-		console.log(adjusted);
-		gst = (adjusted * .05);
-		pst = (adjusted * .07);
+		cc_gross = $('#cc-gross').val();
+		cc_percentage = $('#cc-percentage').val();
+		temp_after_tax = ((gross / 1.12));
+		console.log(temp_after_tax);
+		after_tax = parseFloat(temp_after_tax);
+		console.log(after_tax);
+		gst = (after_tax * .05);
+		pst = (after_tax * .07);
+		temp_cc_fee = cc_gross * cc_percentage / 100
+		cc_fee = parseFloat(temp_cc_fee);
+		adjusted = after_tax - cc_fee;
 		artist = (adjusted * .8);
 		venue = (adjusted * .2);
 		venue_pst = pst;
@@ -31,11 +36,12 @@ $(document).ready(function(){
 		venue_net = venue_gst + venue_pst + venue_20;
 		$('#gst').text('$' + gst.toFixed(2));
 		$('#pst').text('$' + pst.toFixed(2));
-		$('#adjusted').text('$' + adjusted.toFixed(2));
+		$('#after-tax').text('$' + after_tax.toFixed(2));
 		$('#artist').text('$' + artist.toFixed(2));
 		$('#venue').text('$' + venue.toFixed(2));
 		$('#venue-gst').text('$' + venue_gst.toFixed(2));
 		$('#venue-pst').text('$' + venue_pst.toFixed(2));
+		$('#cc-fee').text('$' + cc_fee.toFixed(2));
 		$('#venue-20').text('$' + venue_20.toFixed(2));
 		$('#venue-net').text('$' + venue_net.toFixed(2))
 		$('#artist-gst').text('$' + artist_gst.toFixed(2));
