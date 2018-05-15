0b6b343147351f3d1f5792db89bc8b0929139e89
Josh Briand <joshbriand@gmail.com>
2018-05-15 15:00:08 -0700

    cash 3

--- jbv2/static/js/cash.js
+++ jbv2/static/js/cash.js
@@ -7,14 +7,18 @@ $(document).ready(function(){
 	function calculate() {
 		console.log('calculate');
 		if ($('#hundreds').val()) {
-			hundreds = parseFloat($('#gross').val());
+			hundreds = $('#gross').val();
 		}
 		else {
-			hundreds = 0.00;
+			hundreds = 0;
 		}
 		console.log(hundreds);
-
-
+		hundreds_value = hundreds * 100;
+		console.log(hundreds_value);
+		hundreds_float_value = parseFloat(hundreds * 100);
+		console.log(hundreds_float_value)
+		hundreds_float_value_2 = parseFloat(hundreds * 100).toFixed(2);
+		console.log(hundreds_float_value_2);
 		$('#artist-net').text('$' + artist_net.toFixed(2));
 	}
 
