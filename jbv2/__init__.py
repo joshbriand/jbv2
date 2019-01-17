c09c94811daa39d52bc7a661d530336779620483
Josh Briand <joshbriand@gmail.com>
2019-01-17 14:07:27 -0800

    standings 16

--- jbv2/__init__.py
+++ jbv2/__init__.py
@@ -1563,6 +1563,9 @@ def poolStandings():
         tier1 = [500, 300, 250, 225, 200, 180, 160, 140, 120, 100, 80, 80, 80, 80, 80, 50, 50, 50, 50, 50, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 15]
         tier2 = [350, 250, 200, 175, 150, 125, 100, 90, 80, 70, 50, 50, 50, 50, 50, 40, 40, 40, 40, 40, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 10]
         tier3 = [250, 150, 125, 100, 90, 80, 70, 60, 50, 40, 30, 30, 30, 30, 30, 25, 25, 25, 25, 25, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 5]
+        points[0][0] = 10
+        print points[0]
+        print points[1]
         for choice in choices:
             golferResults = results.filter_by(golfer=choice.golfer).all()
             for golferResult in golferResults:
@@ -1572,9 +1575,6 @@ def poolStandings():
                     points[golferResult.tournament.id - 1][choice.user.id - 1] += tier2[golferResult.overall - 1]
                 elif golferResult.tournament.tier == 3:
                     points[golferResult.tournament.id - 1][choice.user.id - 1] += tier3[golferResult.overall - 1]
-                print golferResult.tournament.id
-                print points[0]
-                print points[1]
         return render_template('pool/standings.html',
                                 user=user.username,
                                 users=users,
